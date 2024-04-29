275eb4f5d3d5d12c45f6ad498f56aace4657d453
Karen Baltazar <a01707750@tec.mx>
2024-04-28 18:09:39 -0600

    [Array] Implement optimized solution for Remove Element problem

--- TopInterview150/array_string/027_remove_element.py
+++ TopInterview150/array_string/027_remove_element.py
@@ -2,21 +2,15 @@ class Solution(object):
     def removeElement(self, nums, val):
         j = len(nums) - 1
         i = 0
-        k = 0
-
+        
         while j >= 0 and j >= i:
             if nums[j] == val:
                 j = j - 1
             elif nums[i] == val:
                 nums[i] = nums[j]
-                nums[j] = val
                 i = i + 1
-                k = k + 1
                 j = j - 1 
             else:
                 i = i + 1
-                k = k + 1 
 
-        diff = len(nums) - k
-        nums = nums[:diff]
-        return k
+        return i
